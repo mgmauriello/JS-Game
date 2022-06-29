34902d85dbbeb0fedec1c1a7d416d90daaad31c7
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-06-29 02:29:40 +0000

    positioned basemap

---
+++ index.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './images/pellet-town.png'
+
+image.onload = () => {
+  c.drawImage(image, -750, -500)
+}
