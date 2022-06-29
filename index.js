37ea6daf04e164854c3b39ae5dde9d2a4e5d7e8c
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-06-29 03:48:30 +0000

    aligned player sprite to map

--- index.js
+++ index.js
@@ -9,6 +9,38 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const image = new Image()
 image.src = './images/pellet-town.png'
 
+const playerImage = new Image()
+playerImage.src = './images/player-down.png'
+
+
 image.onload = () => {
-  c.drawImage(image, -750, -500)
+  c.drawImage(image, -800, -550)
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 2,
+    canvas.height / 2 + playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  )
 }
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'w':
+      console.log('pressed w key')
+      break
+    case 'a':
+      console.log('pressed a key')
+      break
+    case 's':
+      console.log('pressed s key')
+      break
+    case 'd':
+      console.log('pressed d key')
+      break
+  }
+})
