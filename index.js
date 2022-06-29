08f3df807ac9637eac1a93c7854f76515722e354
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-06-29 04:36:47 +0000

    player movement and boundaries

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+// basemap and boundaries
+
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i+= 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+class Boundary {
+  static width = 48
+  static height = 48
+  constructor({position}) {
+    this.position = position
+    this.width = 48
+    this.height = 48
+  }
+
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const boundaries = []
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025) {
+      boundaries.push(
+        new Boundary({position: {
+          x: j * Boundary.width,
+          y: i * Boundary.height
+      }}))
+    }
+  })
+})
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
@@ -32,6 +67,7 @@ const background = new Sprite({
   image: image
 })
 
+// player movement
 const keys = {
   w: {
     pressed: false
@@ -50,6 +86,9 @@ const keys = {
 function animate() {
   window.requestAnimationFrame(animate)
     background.draw()
+    boundaries.forEach(boundary => {
+      boundary.draw()
+    })
     c.drawImage(
       playerImage,
       0,
@@ -61,42 +100,52 @@ function animate() {
       playerImage.width / 4,
       playerImage.height
     )
+
+
+  if (keys.w.pressed && lastKey === 'w') background.position.y += 5
+  else if (keys.a.pressed && lastKey === 'a') background.position.x += 5
+  else if (keys.s.pressed && lastKey === 's') background.position.y -= 5
+  else if (keys.d.pressed && lastKey === 'd') background.position.x -= 5
+
 }
 animate()
+
+let lastKey = ''
 
 window.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'w':
       keys.w.pressed = true
+      lastKey = 'w'
       break
     case 'a':
       keys.a.pressed = true
+      lastKey = 'a'
       break
     case 's':
       keys.s.pressed = true
+      lastKey = 's'
       break
     case 'd':
       keys.d.pressed = true
+      lastKey = 'd'
       break
-  }
-  if (keys.w.pressed) {
-    background.position.y = background.position.y - 3
   }
 })
 
 window.addEventListener('keyup', (e) => {
   switch (e.key) {
     case 'w':
-      keys.w.pressed = true
+      keys.w.pressed = false
       break
     case 'a':
-      keys.a.pressed = true
+      keys.a.pressed = false
       break
     case 's':
-      keys.s.pressed = true
+      keys.s.pressed = false
       break
     case 'd':
-      keys.d.pressed = true
+      keys.d.pressed = false
       break
   }
 })
